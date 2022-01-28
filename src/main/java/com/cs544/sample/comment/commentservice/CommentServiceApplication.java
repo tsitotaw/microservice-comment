1e5e6808996ea470b2bf174e41ac36efc34957f8
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-01-27 19:26:21 -0600

    added eureca server

--- src/main/java/com/cs544/sample/comment/commentservice/CommentServiceApplication.java
+++ src/main/java/com/cs544/sample/comment/commentservice/CommentServiceApplication.java
@@ -2,6 +2,7 @@ package com.cs544.sample.comment.commentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class CommentServiceApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
